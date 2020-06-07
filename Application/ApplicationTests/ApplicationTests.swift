88ed0e45f2f313045892cfc5b25a58c77915f188
Lukas Holmberg <lukazlovicc@hotmail.com>
2020-06-07 16:00:35 +0200

    test case (fail)

---
+++ Application/ApplicationTests/ApplicationTests.swift
@@ -0,0 +1,19 @@
+//
+//  ApplicationTests.swift
+//  ApplicationTests
+//
+//  Created by Lukas Holmberg on 2020-06-07.
+//  Copyright © 2020 Stefan Holmberg. All rights reserved.
+//
+
+import XCTest
+
+class ApplicationTests: XCTestCase {
+    
+    func testHelloWorld() {
+        var helloWorld: String?
+        helloWorld = "dau"
+        XCTAssertNil(helloWorld)
+    }
+
+}
